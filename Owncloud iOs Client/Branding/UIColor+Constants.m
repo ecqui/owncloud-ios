59904629fa8631cb60af81acbf51ddcc94bd2af5
Slav Sarafski <slav@spiritinvoker.com>
2016-03-11 17:15:22 +0200

    First commit

--- Owncloud iOs Client/Branding/UIColor+Constants.m
+++ Owncloud iOs Client/Branding/UIColor+Constants.m
@@ -21,11 +21,11 @@
 
 //Tint color of navigation bar
 + (UIColor*)colorOfNavigationBar{
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+    return [UIColor colorWithRed:0/255.0f green:130/255.0f blue:67/255.0f alpha:1.0];
 }
 //Color of background view in navigation bar, only for iOS 7 for transparency
 + (UIColor*)colorOfBackgroundNavBarImage {
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:0.7];
+    return [UIColor colorWithRed:0/255.0f green:130/255.0f blue:67/255.0f alpha:0.7];
 }
 
 //Color of letters in navigation bar
@@ -64,7 +64,7 @@
 
 //Tint color of tab bar
 + (UIColor*)colorOfTintUITabBar {
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+    return [UIColor colorWithRed:0/255.0f green:130/255.0f blue:67/255.0f alpha:1.0];
 }
 
 //Tint color for selected tab bar item
@@ -131,7 +131,7 @@
 
 //Background color of login button
 +(UIColor *)colorOfLoginButtonBackground{
-    return [UIColor colorWithRed:96/255.0f green:133/255.0f blue:154/255.0f alpha:1.0];
+    return [UIColor colorWithRed:0/255.0f green:130/255.0f blue:67/255.0f alpha:1.0];
 }
 
 //Text color of the text of the login button
