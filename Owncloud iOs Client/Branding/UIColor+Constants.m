8d4b4d106c3ac67b39d53bd67c3b853e7fd69e51
Slav Sarafski <slav@spiritinvoker.com>
2016-07-13 15:18:56 +0300

    Update to 3.5.0

--- Owncloud iOs Client/Branding/UIColor+Constants.m
+++ Owncloud iOs Client/Branding/UIColor+Constants.m
@@ -21,11 +21,11 @@
 
 //Tint color of navigation bar
 + (UIColor*)colorOfNavigationBar{
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+    return [UIColor colorWithRed:1/255.0f green:134/255.0f blue:103/255.0f alpha:1.0];
 }
 //Color of background view in navigation bar
 + (UIColor*)colorOfBackgroundNavBarImage {
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:0.7];
+    return [UIColor colorWithRed:1/255.0f green:134/255.0f blue:103/255.0f alpha:1.0];
 }
 
 //Color of letters in navigation bar
@@ -45,12 +45,12 @@
 
 //Tint color of tool bar
 + (UIColor*)colorOfToolBar{
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+    return [UIColor colorWithRed:1/255.0f green:134/255.0f blue:103/255.0f alpha:1.0];
 }
 
 //Color of background view in toolBar bar, only for iOS 7 for transparency
 + (UIColor*)colorOfBackgroundToolBarImage {
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:0.7];
+    return [UIColor colorWithRed:1/255.0f green:134/255.0f blue:103/255.0f alpha:0.7];
 }
 
 //Tint color of tool bar buttons
@@ -64,18 +64,18 @@
 
 //Tint color of tab bar
 + (UIColor*)colorOfTintUITabBar {
-    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+    return [UIColor colorWithRed:1/255.0f green:134/255.0f blue:103/255.0f alpha:1.0];
 }
 
 //Tint color for selected tab bar item
 + (UIColor*)colorOfTintSelectedUITabBar {
     return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
-
+    
 }
 
 //Tint color for non selected tab bar item (only works with the labels)
 + (UIColor*)colorOfTintNonSelectedUITabBar {
-   return [UIColor colorWithRed:0/255.0f green:0/255.0f blue:0/255.0f alpha:1.0];
+    return [UIColor colorWithRed:0/255.0f green:0/255.0f blue:0/255.0f alpha:1.0];
 }
 
 
@@ -132,7 +132,7 @@
 
 //Background color of login button
 +(UIColor *)colorOfLoginButtonBackground{
-    return [UIColor colorWithRed:96/255.0f green:133/255.0f blue:154/255.0f alpha:1.0];
+    return [UIColor colorWithRed:1/255.0f green:134/255.0f blue:103/255.0f alpha:1.0];
 }
 
 //Text color of the text of the login button
@@ -162,7 +162,7 @@
 
 //Color of webview background
 + (UIColor*)colorOfWebViewBackground{
-   return [UIColor colorWithRed:26/255 green:26/255 blue:28/255 alpha:1.0];
+    return [UIColor colorWithRed:26/255 green:26/255 blue:28/255 alpha:1.0];
 }
 
 //Color of background in detail view when there are not file selected
