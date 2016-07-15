1439cd2e6aadaff341f25ed3b234fd590fc7bc3d
Slav Sarafski <slav@spiritinvoker.com>
2016-07-15 09:18:47 +0300

    Fixed Customisation and added splash screen.

--- Owncloud iOs Client/Branding/Customization.h
+++ Owncloud iOs Client/Branding/Customization.h
@@ -120,8 +120,11 @@
 //Number of uploads shown in recents tab from the database
 #define k_number_uploads_shown 30
 
-//Set text of status bar white only for iOS 7. YES = White | NO = Black
-#define k_is_text_status_bar_white NO
+//Set text of status bar white. YES = White | NO = Black
+#define k_is_text_status_bar_white YES
+
+//Set text of login view status bar white. YES = White | NO = Black
+#define k_is_text_login_status_bar_white NO
 
 //Show the help link on login
 #define k_is_shown_help_link_on_login NO
@@ -138,3 +141,9 @@
 
 //Help Guide init app
 #define k_show_main_help_guide NO
+
+//Show share with users
+#define k_is_share_with_users_available YES
+
+//Show share by link
+#define k_is_share_by_link_available YES
